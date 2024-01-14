3d9ffe0d1bd0ef716d5298596c185828f356aa06
Vikram Singh <mr.vikram004@gmail.com>
2024-01-15 00:47:45 +0400

    basic styling added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import MyPhoto from "./icons/photo.jpg";
 
 const HeaderComponent =() => {
-    return <div className="header">
-        <img className="w-10" src={MyPhoto}></img>
+    return <div className="header" class="container mx-auto">
+        <img class=" w-[90px] h-[160px]" src={MyPhoto}></img>
     </div>
 }
 
 
 const AppLayout =() => {
-    return <div className="app">
+    return <div className="app" class="bg-slate-600">
+        <HeaderComponent/>
+        <HeaderComponent/>
         <HeaderComponent/>
     </div>
 }
